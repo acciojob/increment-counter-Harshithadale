9a1a91781892c5c487f79d3e0eee4b44c1e44a44
Harshithadale <daleharshi6045@gmail.com>
2025-04-01 10:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let pointer = 0
+const counter = document.getElementById("counter");
+const incrementBtn = document.getElementById("incrementBtn");
+incrementBtn.onclick=()=>{
+	pointer = pointer+1
+	alert(pointer)
+	counter.textContent= pointer;
+};
